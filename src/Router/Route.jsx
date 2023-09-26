fd7edebeb779d81498b429a015e730533e71972e
Shimul <shimulmohammad420@gmail.com>
2023-09-27 01:20:46 +0600

    error page added

--- src/Router/Route.jsx
+++ src/Router/Route.jsx
@@ -3,11 +3,13 @@ import MainLayout from "../Layout/MainLayout";
 import Home from "../Pages/Home/Home";
 import Donation from "../Pages/Donation/Donation";
 import Statistics from "../Pages/Statistics/Statistics";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
     {
         path:'/',
         element: <MainLayout></MainLayout>,
+        errorElement: <ErrorPage></ErrorPage>,
         children:[
             {
                 path:'/',
