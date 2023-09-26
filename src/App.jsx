a8b46ac9d608b8cf74cd98ed9aa22e3b1063aab9
Shimul <shimulmohammad420@gmail.com>
2023-09-26 13:21:44 +0600

    creating 12 fake json data

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ function App() {
 
   return (
     <>
-      <h1 className='text-2xl font-semibold'>Vite + React</h1>
-      <h1 className='text-2xl font-semibold'>Vite + React</h1>
-      <h1 className='text-2xl font-semibold'>Vite + React</h1>
     </>
   )
 }
